a18852b38c6c1403ec72a2ffab5cc33b413acee0
Mathias5R <mathiassilva4@gmail.com>
2018-10-28 21:46:22 -0300

    Adding carousel component

--- src/modules/Home/HomeComponent.js
+++ src/modules/Home/HomeComponent.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import styled from 'styled-components';
 import LinearGradient from 'react-native-linear-gradient';
+import Carousel from 'react-native-snap-carousel';
+
+const screenWidth = Dimensions.get(`window`).width;
 
 const Container = styled(LinearGradient)`
   flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
-const HomeComponent = () => <Container colors={[`#23074d`, `#cc5333`]} />;
+const Item = styled.Text`
+  flex: 1;
+  background-color: white;
+  border-radius: 20;
+`;
+
+const CarouselView = styled.View`
+  height: 300;
+`;
+
+const data = [
+  {
+    id: 1,
+    text: `inglês`,
+  },
+  {
+    id: 2,
+    text: `matemática`,
+  },
+  {
+    id: 3,
+    text: `português`,
+  },
+];
+
+const HomeComponent = () => (
+  <Container colors={[`#23074d`, `#cc5333`]}>
+    <CarouselView>
+      <Carousel
+        data={data}
+        renderItem={({ item }) => <Item>{item.text}</Item>}
+        sliderWidth={screenWidth}
+        itemWidth={250}
+      />
+    </CarouselView>
+  </Container>
+);
 
 export default HomeComponent;
