ad066880324fe998b336e564211290e3e725c257
Mathias5R <mathiassilva4@gmail.com>
2018-11-12 21:25:20 -0200

    Adding login button

---
+++ src/modules/Login/LoginButtonComponent.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled, { withTheme } from 'styled-components';
+import { PropTypes } from 'prop-types';
+import LinearGradient from 'react-native-linear-gradient';
+
+const LoginButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 50;
+  background-color: red;
+  border-radius: 20;
+`;
+
+const ButtonGradient = styled(LinearGradient)`
+  flex: 1;
+  border-radius: 20;
+  padding-left: 20;
+  padding-right: 20;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text`
+  color: ${({ theme }) => theme.colors.button_text};
+  font-size: 16;
+`;
+
+const LoginButtonComponent = ({ text }) => (
+  <LoginButton style={{ elevation: 4 }}>
+    <ButtonGradient
+      colors={[`#8a2387`, `#e94057`, `#f27121`]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <Text>{text}</Text>
+    </ButtonGradient>
+  </LoginButton>
+);
+
+LoginButtonComponent.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default withTheme(LoginButtonComponent);
--- src/modules/Login/LoginComponent.js
+++ src/modules/Login/LoginComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import LinearGradient from 'react-native-linear-gradient';
 import FormInputComponent from './FormInputComponent';
+import LoginButtonComponent from './LoginButtonComponent';
 
-const Container = styled(LinearGradient)`
+const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
@@ -11,14 +11,16 @@ const Container = styled(LinearGradient)`
 
 const FormView = styled.View`
   width: 80%;
+  margin-bottom: 50;
 `;
 
 const LoginComponent = () => (
-  <Container colors={[`#8a2387`, `#e94057`, `#f27121`]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}>
+  <Container>
     <FormView>
       <FormInputComponent type="email" placeholder="E-mail" />
       <FormInputComponent type="password" placeholder="Senha" />
     </FormView>
+    <LoginButtonComponent text="Login" />
   </Container>
 );
 
